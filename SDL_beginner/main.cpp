26face29657d28dc94818d8fe75d7221ea805413
Patrick Schurr <PSchurr@Patricks-MacBook-Pro.local>
2015-03-13 16:06:02 -0500

    Initial Commit

---
+++ SDL_beginner/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_beginner
+//
+//  Created by Patrick Schurr on 3/13/15.
+//  Copyright (c) 2015 Patrick Schurr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
